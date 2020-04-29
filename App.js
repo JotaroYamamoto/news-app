f40b0b9293e9aa8bb0ee6048c9dc7d180b871d6a
JotaroYamamoto <kensin2131@gmail.com>
2020-04-29 16:11:09 +0900

    first-commit

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +9,56 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    // 縦並びか横並びか決める
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+          <Image
+            style={{ width: 98, height: 98 }}
+            source={{
+              uri: 'https://picsum.photos/200/300',
+            }}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={3} style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+          <Text style={styles.subText}>Reactnews</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
